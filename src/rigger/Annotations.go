452bc29682ba588d22a2b3d47e09e55177d80252
runningPHPer <462029019@qq.com>
2021-05-20 01:18:52 +0800

    文件配置读取，value注解加入

--- src/rigger/Annotations.go
+++ src/rigger/Annotations.go
@@ -42,7 +42,7 @@ func (this *Value) String() string {
 		return ""
 	}
 	prefix := strings.Split(get_prefix, ".")
-	if config := this.Beanfactory.GetBean(NewSysConfig()); config != nil {
+	if config := this.Beanfactory.GetBean(new(SysConfig)); config != nil {
 		get_value := GetConfigValue(config.(*SysConfig).Config, prefix, 0)
 		if get_value != nil {
 			return fmt.Sprintf("%v", get_value)
